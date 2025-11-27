1239c4cad9a70045a99148165f197ad69f78c7a2
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-27 21:59:22 +0700

    setup rute DestinasiEntry untuk layar input data

---
+++ app/src/main/java/com/example/roomdatabase_pertemuan9/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase_pertemuan9.view.route
+
+import com.example.roomdatabase_pertemuan9.R
+import com.example.roomdatabase_pertemuan9.view.route.DestinasiNavigasi
+
+object DestinasiEntry :DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
