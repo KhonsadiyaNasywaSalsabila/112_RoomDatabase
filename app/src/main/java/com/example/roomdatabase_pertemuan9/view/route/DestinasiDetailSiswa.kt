b687b99e7909d831445d3960eb601aad45c7e536
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-12-04 22:00:11 +0700

    definisikan rute navigasi DestinasiDetailSiswa dengan argumen

---
+++ app/src/main/java/com/example/roomdatabase_pertemuan9/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.roomdatabase_pertemuan9.view.route
+
+import com.example.roomdatabase_pertemuan9.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
