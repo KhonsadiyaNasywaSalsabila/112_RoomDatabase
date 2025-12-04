3f2d695fed3f5f052d5025a59c5301f1833c7f3a
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-12-04 21:57:04 +0700

    registrasi DetailViewModel dan EditViewModel ke dalam PenyediaViewModel

--- app/src/main/java/com/example/roomdatabase_pertemuan9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_pertemuan9/viewmodel/provider/PenyediaViewModel.kt
@@ -6,26 +6,28 @@ import com.example.roomdatabase_pertemuan9.repositori.AplikasiSiswa
 import com.example.roomdatabase_pertemuan9.viewmodel.EntryViewModel
 import com.example.roomdatabase_pertemuan9.viewmodel.HomeViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.roomdatabase_pertemuan9.view.DetailDataSiswa
+import com.example.roomdatabase_pertemuan9.viewmodel.DetailViewModel
+import com.example.roomdatabase_pertemuan9.viewmodel.EditViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
 
-        // HAPUS BARIS INI: val aplikasiSiswa = this.aplikasiSiswa()
-
         initializer {
-            // Pindah variabel ke sini, di mana 'this' adalah CreationExtras
-            val aplikasiSiswa = this.aplikasiSiswa()
-            HomeViewModel(
-                aplikasiSiswa.container.repositoriSiswa
-            )
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
         initializer {
-            // Ulangi untuk EntryViewModel
-            val aplikasiSiswa = this.aplikasiSiswa()
-            EntryViewModel(
-                aplikasiSiswa.container.repositoriSiswa
-            )
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
