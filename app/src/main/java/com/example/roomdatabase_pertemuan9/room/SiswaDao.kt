a8af0e0c5ed8b33ca67aac6b937369dd1df594c9
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-12-04 21:36:46 +0700

    Menambahkan fungsi get untuk mengambil detail satu siswa berdasarkan ID.

--- app/src/main/java/com/example/roomdatabase_pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase_pertemuan9/room/SiswaDao.kt
@@ -1,15 +1,22 @@
 package com.example.roomdatabase_pertemuan9.room
 import  androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
-    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert (onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
+
 }
